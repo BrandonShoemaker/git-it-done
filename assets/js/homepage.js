9bb7fb270994a4dc2c7b04f16e1c63204949604f
BrandonShoemaker <brandon.shoemaker2080@gmail.com>
2021-03-17 14:11:32 -0500

    Add >30 issue link

---
+++ assets/js/homepage.js
@@ -0,0 +1,57 @@
+function displayRepos(repo, searchTerm){
+    $("#repo-search-form").text(searchTerm);
+    var repoContainer = $("#repos-container");
+
+    for (let i = 0; i < repo.length; i++) {
+        var repoName = repo[i].owner.login + "/" + repo[i].name;
+        var repoEl = $("<div>").addClass("list-item flex-row justify-space-between align-center");
+        var repoNameEl = $("<span>").text(repoName);
+        var statusEl = $("<span>").addClass("flex-row align-center");
+        if(repo[i].open_issues_count > 0){
+            statusEl.html("<i class='fas fa-times status-icon icon-danger'></i>" + repo[i].open_issues_count + " issue(s)");
+        }
+        else if(repo[i].open_issues_count === 0){
+            statusEl.text("There are no repositories.");
+        }
+        else{
+            statusEl.html("<i class='fas fa-check-square status-icon icon-success'></i>");
+        }
+
+        repoEl.append(repoNameEl, statusEl);
+        repoContainer.append(repoEl);
+    }
+}
+
+function getUserRepos(user){
+    var apiUrl = "https://api.github.com/users/"+user+"/repos";
+    fetch(apiUrl).then(response => {
+        if(response.ok){
+            response.json().then(data => {
+                displayRepos(data, user);
+            });
+        }
+        else{
+            response.json().then(errorMessage => {
+                alert("Error: " + errorMessage.message);
+            });
+        }
+    })
+    .catch((error) => {
+        alert("Unable to connect to github");
+    });
+
+}
+
+$(document).ready(function () {
+    $("#user-form").on("click", "button", () => {
+        var username = $("#username");
+        if(!username.val().trim()){
+            alert("Please enter a valid username!");
+            return;
+        }
+        else{
+            getUserRepos(username.val().trim());
+            username.val("");
+        }
+    });
+});
---
+++ assets/js/single.js
@@ -0,0 +1,63 @@
+function displayWarning(repo){
+    var limitWarningEl = $("#limit-warning");
+    limitWarningEl.text("To view more than 30 issues, visit ");
+
+    var moreIssueLinkEl = $("<a>");
+    moreIssueLinkEl.text("See more issues at Github.com");
+    moreIssueLinkEl.attr("href", "https://github.com/" + repo + "/issues");
+    moreIssueLinkEl.attr("target", "_blank");
+
+    limitWarningEl.append(moreIssueLinkEl);
+}
+
+function displayIssues(issues){
+    for (let i = 0; i < issues.length; i++) {
+        var issueEl = $("<a>");
+        issueEl.addClass("list-item flex-row justify-space-between align-center");
+        issueEl.attr("href", issues[i].html_url);
+        issueEl.attr("target", "_blank");
+        
+        var titleEl = $("<span>").text(issues[i].title.trim());
+        issueEl.append(titleEl);
+
+        var typeEl = $("<span>");
+        if(issues.length <= 0){
+            $("#issues-container").text("This repo has no issues!");
+        }
+        else{
+            if(issues[i].pull_request){
+                typeEl.text("(Pull Request)");
+            }
+            else{
+                typeEl.text("(Issue)");
+            }
+            issueEl.append(typeEl);
+            $("#issues-container").append(issueEl);
+        }
+    }
+}
+
+function getRepoIssues(repo){
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    fetch(apiUrl).then((response) => {
+        if(response.ok){
+            response.json().then((issues) => {
+                displayIssues(issues);
+            });
+            if(response.headers.get("link")){
+                console.log("Has more the 30 issues");
+                displayWarning(repo);
+            }
+        }
+        else{
+            response.json().then((result) => {
+                alert("Error: " + result.message);
+            });
+        }
+    })
+    .catch((error) => {
+        alert("Unable to connect to Github");
+    });
+}
+
+getRepoIssues("facebook/react");
